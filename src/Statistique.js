965b76b02f46fb63d33b44909d3e121bcd0db6f2
Adji <nthiamadji39@gmail.com>
2026-04-19 16:20:27 +0000

    Finalisation des exercices : ajout Footer, Date et Statistiques

---
+++ src/Statistique.js
@@ -0,0 +1,13 @@
+
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="stat-card">
+      <h3 className="stat-chiffre">10</h3>
+      <p className="stat-label">Lignes de bus</p>
+    </div>
+  );
+}
+
+export default Statistique;
